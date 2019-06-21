8d79eb2bbcc94fb4ffe51a6f6599cd31f72f111d
YAN FANG <49136056+YanFangBio@users.noreply.github.com>
2019-06-20 20:49:06 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,8 +1,8 @@
 library(readr)
 library( stringr)
-power <- read_csv2( file = file )
 file <- "household_power_consumption.txt"
-power <- power[power$Date == c("2/1/2007", "2/2/2007"),]
+power <- read_csv2( file = file )
+power <- power[power$Date == c("1/2/2007", "2/2/2007"),]
 
 png(file = "plot1.png", bg = "white")
 hist(as.numeric(power$Global_active_power), 
